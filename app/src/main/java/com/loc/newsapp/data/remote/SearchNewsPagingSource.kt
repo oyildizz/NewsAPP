617e47b6028123181f90b2f65d892d77cfcb7bdd
oyildizz <oyildizz@icloud.com>
2024-01-15 19:00:28 +0300

    first push

---
+++ app/src/main/java/com/loc/newsapp/data/remote/SearchNewsPagingSource.kt
@@ -0,0 +1,4 @@
+package com.loc.newsapp.data.remote
+
+class SearchNewsPagingSource {
+}
---
+++ app/src/main/java/com/loc/newsapp/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.loc.newsapp.util
+
+object Constants {
+    const val USER_SETTINGS="userSettings"
+
+    const val APP_ENTRY ="appEntry"
+
+    const val API_KEY="47e4ca17c9304856a5ac789ba61310e6"
+
+    const val BASE_URL = "https://newsapi.org/v2/"
+}
